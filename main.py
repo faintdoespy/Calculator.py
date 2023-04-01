a3471b20cdf496e217e01a18584fa22f702b6102
faint <129570987+faintdoespy@users.noreply.github.com>
2023-04-01 02:53:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from time import sleep as wait
+from colorama import Fore
 import os
 
 def clear():
     os.system('cls')
 
 def calculator():
-    wait(1)
     clear()
-    num1 = int(input(f'Number 1: '))
-    num2 = int(input(f'Number 2: '))
-    operation = input(f'+, -, /, or %: ')
-    if operation == str('+'):
-        print(f'{str(num1+num2)}')
-    if operation == str('-'):
-        print(f'{str(num1-num2)}')
-    if operation == str('/'):
-        print(f'{str(num1/num2)}')
-    if operation == str('%'):
-        print(f'{str(num1%num2)}')
+    while True:
+        print(Fore.LIGHTMAGENTA_EX)
+        wait(1)
+        num1 = int(input(f'Number 1: '))
+        num2 = int(input(f'Number 2: '))
+        operation = input(f'+, -, /, or %: ')
+        if operation == str('+'):
+            print(f'{str(num1+num2)}')
+        if operation == str('-'):
+            print(f'{str(num1-num2)}')
+        if operation == str('/'):
+            print(f'{str(num1/num2)}')
+        if operation == str('%'):
+            print(f'{str(num1%num2)}')
 
 calculator()
